82fca9efa2f4962911e7356aac69c7187e9ab50c
Nellymax <nellymaxencia@gmail.com>
2024-10-30 19:42:33 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,60 @@
+Python 3.12.7 (tags/v3.12.7:0b05ead, Oct  1 2024, 03:06:41) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+3
+5
+>>> x=2
+>>> x+3
+5
+>>> y=3
+>>> x+y
+5
+>>> x=93
+>>> x+y
+96
+>>> x
+93
+>>> 
+>>> _+y
+96
+>>> _
+96
+>>> _+y
+99
+>>> name='maxensia'
+>>> name
+'maxensia'
+>>> name+'nelly'
+'maxensianelly'
+>>> name[0]
+'m'
+>>> name[7]
+'a'
+>>> 
+>>> name[-5]
+'e'
+>>> name[-3]
+'s'
+>>> name[-6]
+'x'
+>>> name[1:4]
+'axe'
+>>> name[0:2]
+'ma'
+>>> name[1:]
+'axensia'
+name[:6]
+'maxens'
+name[3:11]
+'ensia'
+'beauty'+name[0:]
+'beautymaxensia'
+
+
+my love='puppies'
+SyntaxError: invalid syntax
+mydog ='cool'
+len(mydog)
+4
+mylove ='puppies'
+len(mylove)
+7
