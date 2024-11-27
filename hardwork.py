b585ef92366d1a2cb56e0063d6495ca06a32f663
Nellymax <nellymaxencia@gmail.com>
2024-11-27 16:42:40 +0300

    Add files via upload

---
+++ hardwork.py
@@ -0,0 +1,54 @@
+hardwork.py
+#variables with intergers
+
+print(2+3)
+
+x=2
+print(x+4)
+
+y=3
+print(x+y)
+
+x=9
+print(x+y)
+
+print(x)
+
+print(x+10)
+
+# variables with strings
+
+name='maxensia'
+print(name)
+
+print (name + 'nelly')
+ 
+print(name[0])
+
+print(name[7])
+
+print(name[-6])
+
+print(name[-2])
+
+#wen obtaining numerous characters, we strat with the charater we want to the character we want to end like print(name[0:7])
+
+print(name[0:3])
+
+print(name[1:])
+
+print(name[:7])
+
+print('my name is' + name[0:3])
+
+#wen finding the length of characters
+#the command is "print(len())"
+
+mylove = "God"
+print(len(mylove))     #the name "God" has 3 letters in length.
+
+MTN = "auntieGertrude"   #the length is 14 characters
+print(len(MTN))   
+
+MTN = "auntie Gertrude"   #the length is 15 characters coz of the space between auntie and Gertrude 
+print(len(MTN))   
